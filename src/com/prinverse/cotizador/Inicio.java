6efc01e2751c08947a9b8240c0e2b7267a5be277
Jaime Carmona <jaimecgab@gmail.com>
2026-07-30 19:16:32 -0600

    Update Inicio.java

--- src/com/prinverse/cotizador/Inicio.java
+++ src/com/prinverse/cotizador/Inicio.java
@@ -5,7 +5,7 @@ public class Inicio {
 	public static void main(String[] args) {
 
 		System.out.println("=== CotizadorLite V3 ===");
-		System.out.println("inventario de productos");
+		System.out.println("inventario de productos nuevos");
 
 
 	}
