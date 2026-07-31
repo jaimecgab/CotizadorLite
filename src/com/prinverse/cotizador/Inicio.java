1909b9479a635b6c5066a8729e0b177fd2833d24
Jaime Carmona <jaimecgab@gmail.com>
2026-07-30 18:46:26 -0600

    union de inventarios y productos

--- src/com/prinverse/cotizador/Inicio.java
+++ src/com/prinverse/cotizador/Inicio.java
@@ -3,8 +3,10 @@ package com.prinverse.cotizador;
 public class Inicio {
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenido a CotizadorLite");
-		System.out.println("inventario");
+
+		System.out.println("=== CotizadorLite ===");
+		System.out.println("inventario de productos");
+
 
 	}
 
