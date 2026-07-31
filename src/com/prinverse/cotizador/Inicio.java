3741157a9fb8059c97e570f9456960279907338c
Jaime Carmona <jaimecgab@gmail.com>
2026-07-30 18:54:45 -0600

    Prueba reset mixed

--- src/com/prinverse/cotizador/Inicio.java
+++ src/com/prinverse/cotizador/Inicio.java
@@ -4,7 +4,7 @@ public class Inicio {
 
 	public static void main(String[] args) {
 
-		System.out.println("=== CotizadorLite ===");
+		System.out.println("=== CotizadorLite V3 ===");
 		System.out.println("inventario de productos");
 
 
